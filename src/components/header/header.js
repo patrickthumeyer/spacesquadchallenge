192e86b9d65ed9072680850693b2b7218c101cdd
patrick.thumeyer <p.thumeyer@idealo.de>
2021-03-22 13:30:24 +0100

    adds svg logo to dashboad

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./header.scss";
-import CoronaSVG from "../../assets/svgs/corona";
 
 const Header = ({ headline }) => {
   return (
     <div className="header">
       <h1 className="header__text">{headline}</h1>
-      <div className={"header__image"}>
-        <CoronaSVG />
-      </div>
+      <div className="header__regionDropbown"></div>
     </div>
   );
 };
